437725960b25930ce83ccbfa42d406bf407d4116
Jacob Middag <jacob@gaddim.nl>
2020-06-05 08:19:25 +0200

    Version container images

--- build.sh
+++ build.sh
@@ -10,9 +10,15 @@ for part in base zlib pcre bash coreutils findutils git grep; do
     file="Dockerfile_$part"
     if [[ -n $1 && $1 != "$part" ]]; then continue; fi
     docker build --file "$file" --tag "middagj/kaniko-$part" .
+    tag="$(sed -nE 's/^FROM [^:]+:([^ ]+)/\1/p' Dockerfile | tail -n 1)"
 done
 
 if [[ -z $1 ]]; then
-    docker build --tag middagj/kaniko .
-    docker push middagj/kaniko:latest
+    tag="$(sed -nE 's/^FROM [^:]+:([^ ]+)/\1/p' Dockerfile | tail -n 1)"
+    docker build --tag "middagj/kaniko:$tag" .
+    docker push "middagj/kaniko:$tag"
+    if [[ "$(git rev-parse --abbrev-ref HEAD)" == 'master' ]]; then
+        docker tag "middagj/kaniko:$tag" "middagj/kaniko:latest"
+        docker push "middagj/kaniko:latest"
+    fi
 fi
